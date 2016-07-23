b14d4fd276a6bcd289b1ebd8ad229ed2d8e959e5
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-23 12:45:34 -0400

    Added jQuery to home.controller, added ng-mouseover and ng-mouseleave functions to home.controller and home.tpl.html, enabled submission and injection of images in addedit.controller and addedit.tpl.html

--- src/js/controllers/addedit.controller.js
+++ src/js/controllers/addedit.controller.js
@@ -1,6 +1,12 @@
-function AddEditController ($scope, SERVER, $http){
+function AddEditController ($scope, SERVER, $http, $state){
+
+	$scope.addImage = (image) => {
+	    $http.post(SERVER.URL, image).then( (res) => {
+	      $state.go('root.home');
+	    });
+  	};
 
 }
 
-AddEditController.$inject = ['$scope', 'SERVER', '$http'];
+AddEditController.$inject = ['$scope', 'SERVER', '$http', '$state'];
 export { AddEditController };
--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -1,14 +1,38 @@
+import $ from 'jquery';
+
 function HomeController($scope, SERVER, $http){
-	  $scope.pics = [];
+	  $scope.images = [];
+
+	  	$scope.popups = (image)=>{
+		  $(`${image._id}`).append(`<span class="tag is-danger">Delete
+		  	<button class="delete" ng-click="deleteimage(image._id)"></button>
+		  </span>`)
+		};
+		$scope.removePopups = (image)=>{
+			$(".tag").remove();
+		}
 
   init();
 
+
+
   function init () {
     $http.get(SERVER.URL).then( (res) => {
-      $scope.pics = res.data;
+      $scope.images = res.data;
+      console.log($scope.images);
     });
   }
+
+	$scope.deleteImage = (imageID) =>{
+		console.log(imageID);
+		$http.delete(SERVER.URL + imageID).then( (response) => {
+			$scope.images = $scope.images.filter((i)=>{
+				return i._id !== imageID;
+			});
+		});
+	};	
 }
 
 HomeController.$inject = ['$scope', 'SERVER', '$http'];
+
 export { HomeController };
